438bb31996e84930df127913ebf6e78f3dacbbc4
Henry Shiu <henry_shiu@trend.com.tw>
2020-01-03 17:58:37 +0800

    Add example API

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +14,17 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	
+	@RestController
+    class SimpleController {
+
+        @GetMapping
+        public Map<String, String> hello() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("say", "hello");
+            return map;
+        }
+    }
 
 }
